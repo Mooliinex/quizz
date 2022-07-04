cf6274483b5ec58c2a4c3f926252927c64c8cdfc
Fabien Le Fichoux <fabien.le-fichoux@epitech.eu>
2022-07-04 11:28:03 +0200

    push categorie & question

--- quizz/src/Entity/Categorie.php
+++ quizz/src/Entity/Categorie.php
@@ -2,32 +2,37 @@
 
 namespace App\Entity;
 
+use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Categorie
- *
- * @ORM\Table(name="categorie")
- * @ORM\Entity
- * @ORM\Entity(repositoryClass="App\Repository\CategorieRepository")
+ * @ORM\Entity(repositoryClass=CategorieRepository::class)
  */
 class Categorie
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
      */
     private $id;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=100, nullable=false)
+     * @ORM\Column(type="string", length=255)
      */
     private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Question::class, mappedBy="categorie")
+     */
+    private $questions;
+
+    public function __construct()
+    {
+        $this->questions = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -46,5 +51,38 @@ class Categorie
         return $this;
     }
 
+    /**
+     * @return Collection|Question[]
+     */
+    public function getQuestions(): Collection
+    {
+        return $this->questions;
+    }
+
+    public function addQuestion(Question $question): self
+    {
+        if (!$this->questions->contains($question)) {
+            $this->questions[] = $question;
+            $question->setCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuestion(Question $question): self
+    {
+        if ($this->questions->removeElement($question)) {
+            // set the owning side to null (unless already changed)
+            if ($question->getCategorie() === $this) {
+                $question->setCategorie(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function __toString() {
+        return $this->name;
+    }
 
 }
